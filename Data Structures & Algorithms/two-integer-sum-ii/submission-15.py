63eefd83cfdc98680db3490e304c6d4d22e21d4e
SirReinz <125225403+SirReinz@users.noreply.github.com>
2026-06-23 21:44:43 +1000

    Add: two-integer-sum-ii - submission-15

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-15.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i, n in enumerate(numbers):
+            j = i + 1
+            print("j:",j)
+            while j < len(numbers):
+                if numbers[j] + n > target:
+                    break
+                print("total =", numbers[j] + n, numbers[j], "+", n, "target:", target)
+                if numbers[j] + n == target:
+                    return [i + 1, j + 1]
+                j += 1
